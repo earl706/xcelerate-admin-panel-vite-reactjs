f7388bfffacef10808f611dd7694e043f3981cb0
kylo ren <oneearl706@gmail.com>
2024-11-09 23:38:44 +0800

    Updated sidenav layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -22,12 +22,53 @@ export default function Layout({ children }) {
   ];
 
   return (
-    <div className="flex min-h-screen bg-gray-100">
-      <aside className="w-64 h-screen fixed bg-blue-600 text-white flex flex-col">
-        <div className="p-4 text-xl font-bold border-b border-blue-500">
+    // <div className="flex min-h-screen">
+    //   <aside className="w-64 h-screen sticky top-0  text-white">
+    //     <div className="h-screen my-4 ml-4 flex flex-col text-blue-900 bg-white rounded-[5px] drop-shadow-lg">
+    //       <div className="p-4 text-l text-left font-bold border-b border-blue-500">
+    //         EXCELERATE ADMIN
+    //       </div>
+    //       <nav className="flex-grow">
+    //         <ul>
+    //           {navigationItems.map((navItem, index) => (
+    //             <li key={index}>
+    //               <NavLink
+    //                 to={navItem.link}
+    //                 className={({ isActive }) =>
+    //                   isActive
+    //                     ? "block p-4 my-1 rounded-[5px]  bg-blue-700 text-white"
+    //                     : "block p-4 my-1 rounded-[5px] hover:bg-blue-700 hover:text-white "
+    //                 }
+    //               >
+    //                 {navItem.navItem}
+    //               </NavLink>
+    //             </li>
+    //           ))}
+    //         </ul>
+    //       </nav>
+    //       <div className="border-t border-blue-500">
+    //         <NavLink
+    //           to="/"
+    //           onClick={() => {
+    //             localStorage.clear();
+    //           }}
+    //           className="block p-4 my-1 hover:bg-blue-700 hover:text-white rounded-[5px]"
+    //         >
+    //           Logout
+    //         </NavLink>
+    //       </div>
+    //     </div>
+    //   </aside>
+    //   <main className="flex-grow p-8 min-w-64">
+    //     <div className="p-6 w-[900px]">{children}</div>
+    //   </main>
+    // </div>
+    <div className="flex min-h-screen">
+      <aside className="w-64 h-[calc(100vh-2rem)] my-4 ml-4 px-2 flex sticky top-4 flex-col text-blue-900">
+        <div className="p-4 text-l text-center font-bold mb-4 bg-white rounded-[5px] drop-shadow-lg overflow-hidden">
           EXCELERATE ADMIN
         </div>
-        <nav className="flex-grow">
+        <nav className="flex-grow font-semibold px-2 drop-shadow-lg overflow-hidden mb-4">
           <ul>
             {navigationItems.map((navItem, index) => (
               <li key={index}>
@@ -35,8 +76,8 @@ export default function Layout({ children }) {
                   to={navItem.link}
                   className={({ isActive }) =>
                     isActive
-                      ? "block p-4 bg-blue-700"
-                      : "block p-4 hover:bg-blue-700"
+                      ? "block p-4 my-2 rounded-[5px] bg-blue-700 text-white"
+                      : "transition block p-4 my-2 rounded-[5px] hover:bg-blue-700 hover:text-white"
                   }
                 >
                   {navItem.navItem}
@@ -45,20 +86,20 @@ export default function Layout({ children }) {
             ))}
           </ul>
         </nav>
-        <div className="p-4 border-t border-blue-500">
+        <div className=" overflow-hidden">
           <NavLink
             to="/"
             onClick={() => {
               localStorage.clear();
             }}
-            className="block p-4 hover:bg-blue-700"
+            className="transition font-semibold block p-4 my-2 hover:bg-blue-700 hover:text-white rounded-[5px]"
           >
             Logout
           </NavLink>
         </div>
       </aside>
-      <main className="flex-grow ml-64 p-8 min-w-64">
-        <div className="p-6">{children}</div>
+      <main className="flex-grow p-8 w-[1134px]">
+        <div className="p-6 ">{children}</div>
       </main>
     </div>
   );
